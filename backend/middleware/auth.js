b2155aed8cf772d4c3a3bfc5ff9d7d3eb6266c20
worari <voradorn.aree@gmail.com>
2025-04-10 08:10:57 +0700

    delete

--- backend/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-// backend/middleware/auth.js
-module.exports = (req, res, next) => {
-    const token = req.headers.authorization;
-    if (token !== 'your_secret_token') {
-      return res.status(403).json({ error: 'Unauthorized' });
-    }
-    next();
-  };
-  
